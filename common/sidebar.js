913aeee84c87c63fbd2a8e600ae792d941320267
lovetk253 <lovetk253@gmail.com>
2020-05-18 23:45:24 +0700

    Add files via upload

---
+++ common/sidebar.js
@@ -0,0 +1,49 @@
+$(function() {
+
+  $(".sidebar-dropdown > a").click(function () {
+    $(".sidebar-submenu").slideUp(200);
+    if (
+      $(this)
+        .parent()
+        .hasClass("active")
+    ) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .parent()
+        .removeClass("active");
+    } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .next(".sidebar-submenu")
+        .slideDown(200);
+      $(this)
+        .parent()
+        .addClass("active");
+    }
+  });
+
+  $("#close-sidebar").click(function () {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function () {
+    $(".page-wrapper").addClass("toggled");
+  });
+  loadDataUser();
+  
+});
+function loadDataUser(){
+  var data = JSON.parse(localStorage.getItem('storageUserData'));
+  var fullName = data["name"]["fullName"];
+  var primaryEmail = data["primaryEmail"];
+  var imageUrl = data["imageUrl"];
+  $('#fullName').html(fullName);
+  $('#primaryEmail').html(primaryEmail);
+  $("#imageUrl").attr("src", imageUrl);
+  // document.getElementById("fullName").innerHTML = fullName;
+  // document.getElementById("primaryEmail").innerHTML = primaryEmail;
+  // document.getElementById("imageUrl").src = imageUrl;
+}
+function gotoOrgUnit(){
+  window.location.replace("orgunits.html");
+
+}
